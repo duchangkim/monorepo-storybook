6a14a82f6f21b2359332da9bd749e3389e92f743
duchangkim <duchang.dev@gmail.com>
2022-11-18 12:13:25 +0900

    fix: change storybook title

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # 모노레포 - 
+
+npx chromatic --project-token=d712a34106a1
--- libs/projects/duse/example/pc/ui/sample/src/lib/Sample.tsx
+++ libs/projects/duse/example/pc/ui/sample/src/lib/Sample.tsx
@@ -3,8 +3,8 @@ import styles from './Sample.module.scss';
 /* eslint-disable-next-line */
 export interface SampleProps {}
 
-export function Sample(props: SampleProps) {
-  return <div className={styles['root']}>SAMPLE</div>;
-}
+export const Sample = (props: SampleProps) => {
+  return <div className={styles['root']}>SAMPLE!</div>;
+};
 
 export default Sample;
