b230554eede6d8da3e3e79615845677cd079b57b
duchangkim <duchang.dev@gmail.com>
2022-11-21 18:02:11 +0900

    test: chormatic

--- libs/storybook/host/src/lib/StorybookHost.tsx
+++ libs/storybook/host/src/lib/StorybookHost.tsx
@@ -6,7 +6,7 @@ export interface StorybookHostProps {}
 export function StorybookHost(props: StorybookHostProps) {
   return (
     <div className={styles['container']}>
-      <h1>Welcome to StorybookHost! HAHA!333333</h1>
+      <h1>Welcome to StorybookHost! HAHA!3333333</h1>
     </div>
   );
 }
