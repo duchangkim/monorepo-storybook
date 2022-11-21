e56c74e8d38607483262eedfbb79f794013eaea2
duchangkim <duchang.dev@gmail.com>
2022-11-21 16:59:19 +0900

    add: chromatic publish

--- libs/projects/duse/example/mobile/ui/text-field/src/lib/TextField.tsx
+++ libs/projects/duse/example/mobile/ui/text-field/src/lib/TextField.tsx
@@ -3,7 +3,7 @@ import styles from './TextField.module.scss';
 export interface TextFieldProps {}
 
 const TextField = (props: TextFieldProps) => {
-  return <input className={styles['root']} type="text" placeholder='입력해' />;
+  return <input className={styles['root']} type="text" placeholder='입력해2' />;
 };
 
 export default TextField;
--- libs/projects/duse/example/pc/ui/sample-badge/src/lib/SampleBadge.tsx
+++ libs/projects/duse/example/pc/ui/sample-badge/src/lib/SampleBadge.tsx
@@ -4,5 +4,5 @@ import styles from './SampleBadge.module.scss';
 export interface SampleBadgeProps {}
 
 export const SampleBadge = (props: SampleBadgeProps) => {
-  return <div className={styles['root']}>SampleBadge</div>;
+  return <div className={styles['root']}>SampleBadge2</div>;
 };
