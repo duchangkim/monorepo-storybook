7f025e2bc3629ba97a3df2686008bf5e983edc7e
duchangkim <duchang.dev@gmail.com>
2022-11-21 17:10:51 +0900

    chore: fix workflow file

--- libs/projects/duse/example/mobile/ui/text-field/src/lib/TextField.tsx
+++ libs/projects/duse/example/mobile/ui/text-field/src/lib/TextField.tsx
@@ -3,7 +3,7 @@ import styles from './TextField.module.scss';
 export interface TextFieldProps {}
 
 const TextField = (props: TextFieldProps) => {
-  return <input className={styles['root']} type="text" placeholder='입력해3' />;
+  return <input className={styles['root']} type="text" placeholder='입력해33' />;
 };
 
 export default TextField;
--- libs/storybook/host/src/lib/StorybookHost.tsx
+++ libs/storybook/host/src/lib/StorybookHost.tsx
@@ -6,7 +6,7 @@ export interface StorybookHostProps {}
 export function StorybookHost(props: StorybookHostProps) {
   return (
     <div className={styles['container']}>
-      <h1>Welcome to StorybookHost!</h1>
+      <h1>Welcome to StorybookHost! haha</h1>
     </div>
   );
 }
