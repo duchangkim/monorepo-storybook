9699e33277a80a40b0261953df19cf27111b88d2
duchangkim <duchang.dev@gmail.com>
2022-11-21 17:05:00 +0900

    test: chromatic test

--- libs/projects/duse/example/mobile/ui/text-field/src/lib/TextField.tsx
+++ libs/projects/duse/example/mobile/ui/text-field/src/lib/TextField.tsx
@@ -3,7 +3,7 @@ import styles from './TextField.module.scss';
 export interface TextFieldProps {}
 
 const TextField = (props: TextFieldProps) => {
-  return <input className={styles['root']} type="text" placeholder='입력해2' />;
+  return <input className={styles['root']} type="text" placeholder='입력해3' />;
 };
 
 export default TextField;
--- libs/projects/duse/example/pc/ui/sample/src/lib/Sample.tsx
+++ libs/projects/duse/example/pc/ui/sample/src/lib/Sample.tsx
@@ -4,7 +4,7 @@ import styles from './Sample.module.scss';
 export interface SampleProps {}
 
 export const Sample = (props: SampleProps) => {
-  return <div className={styles['root']}>SAMPLE</div>;
+  return <div className={styles['root']}>SAMPLE2</div>;
 };
 
 export default Sample;
