ffcfc364b484d49c4d04bfa57f7fa9ae4ebc2d2c
duchangkim <duchang.dev@gmail.com>
2022-11-21 18:21:11 +0900

    test: chormatic

--- libs/projects/duse/example/mobile/ui/text-field/src/lib/TextField.tsx
+++ libs/projects/duse/example/mobile/ui/text-field/src/lib/TextField.tsx
@@ -3,7 +3,7 @@ import styles from './TextField.module.scss';
 export interface TextFieldProps {}
 
 const TextField = (props: TextFieldProps) => {
-  return <input className={styles['root']} type="text" placeholder='입력해3333223' />;
+  return <input className={styles['root']} type="text" placeholder='입력해33332223' />;
 };
 
 export default TextField;
